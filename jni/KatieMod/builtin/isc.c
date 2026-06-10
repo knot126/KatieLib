aeeb46585c4d709307fc5afc32b06a950f1acab5
knot126 <cddepppp256@proton.me>
2026-06-10 18:00:29 +0000

    YIP ITS knOwnHandle and knCall updates

--- jni/KatieMod/builtin/isc.c
+++ jni/KatieMod/builtin/isc.c
@@ -18,21 +18,51 @@ enum {
 };
 
 int knCall(lua_State *L) {
+	/**
+	 * [result1, [result2, [...]]] = knCall(script, function, [...])
+	 * 
+	 * Call a function in the given script and return the results.
+	 * 
+	 * :param script: may be one of the constants KN_MENU, KN_MOVIE, KN_HUD or
+	 *                a light userdata pointing to a lua_State obtained with
+	 *                knOwnHandle().
+	 */
+	
 	const int nargs = lua_gettop(L) - 2;
-	const int script_id = luaL_checkinteger(L, 1);
 	const char * const function_name = lua_tostring(L, 2);
 	
 	if (!function_name) {
 		return luaL_error(L, "function name must be a string");
 	}
 	
-	const Scene * const scene = (&gGame->menuScene)[script_id - 1];
+	lua_State *M = NULL;
 	
-	if (!scene) {
-		return luaL_error(L, "scene %d is not loaded", script_id);
+	switch (lua_type(L, 1)) {
+		case LUA_TNUMBER: {
+			const int script_id = lua_tointeger(L, 1);
+			
+			if (script_id < 1 || script_id > 3) {
+				return luaL_error(L, "scene index %d does not refer to a valid scene", script_id);
+			}
+			
+			const Scene * const scene = (&gGame->menuScene)[script_id - 1];
+			
+			if (!scene) {
+				return luaL_error(L, "scene index %d is not loaded", script_id);
+			}
+			
+			M = *scene->script.state;
+			break;
+		}
+		case LUA_TLIGHTUSERDATA: {
+			M = lua_touserdata(L, 1);
+			break;
+		}
+		default: {
+			return luaL_error(L, "script must be a light userdata pointing to a script or a scene enum (one of KN_MENU, KN_MOVIE, KN_HUD)");
+			break;
+		}
 	}
-	
-	lua_State *M = *scene->script.state;
 	
 	// We'll be using this to find how many return values there are and restore
 	// the old stack top
@@ -43,7 +73,7 @@ int knCall(lua_State *L) {
 	
 	if (!lua_isfunction(M, -1)) {
 		lua_pop(M, 1);
-		return luaL_error(L, "inter-script call to script %d failed because %s is not a function", script_id, function_name);
+		return luaL_error(L, "inter-script call to script <%p> failed because %s is not a function", M, function_name);
 	}
 	
 	// Copy call args
@@ -70,8 +100,14 @@ int knCall(lua_State *L) {
 	}
 }
 
+int knOwnHandle(lua_State *L) {
+	lua_pushlightuserdata(L, L);
+	return 1;
+}
+
 int knEnableIsc(lua_State *L) {
 	knRegisterFunc(L, knCall);
+	knRegisterFunc(L, knOwnHandle);
 	
 	knLuaPushEnum(L, KN_MENU);
 	knLuaPushEnum(L, KN_MOVIE);
