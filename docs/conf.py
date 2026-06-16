9486c2ea5a8d9d89c6b433cc9bcd03389cb2cdb6
knot126 <cddepppp256@proton.me>
2026-06-16 04:52:29 +0000

    Added docs

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+from pathlib import Path
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'KatieLib'
+copyright = '2026, knot126'
+author = 'knot126'
+release = Path('../RELEASE').read_text().strip()
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+	'sphinx_lua_ls',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Lua Settings ------------------------------------------------------------
+
+primary_domain = 'lua'
+lua_ls_backend = 'disable'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'nature'
+html_static_path = ['_static']
