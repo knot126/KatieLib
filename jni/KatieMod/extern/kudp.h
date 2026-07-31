561b06d1405bebf1e4c1d3cd13a66468132bd1fd
knot126 <cddepppp256@proton.me>
2026-07-31 05:25:49 +0000

    kudp lib

---
+++ jni/KatieMod/extern/kudp.h
@@ -0,0 +1,110 @@
+#ifndef _KUDP_INCLUDE_
+#define _KUDP_INCLUDE_
+
+#include <sys/socket.h>
+#include <netinet/ip.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#ifndef KUDP_MAX_SIZE
+#define KUDP_MAX_SIZE 65507
+#endif
+
+typedef struct kudp_buffer {
+	unsigned short size;
+	unsigned char data[KUDP_MAX_SIZE];
+} kudp_buffer;
+
+typedef struct kudp_socket {
+	int fd;
+	kudp_buffer buffer;
+} kudp_socket;
+
+kudp_socket *kudp_connect(const char *address, unsigned short port, bool use_connect);
+kudp_buffer *kudp_recieve(kudp_socket *self);
+bool kudp_send(kudp_socket *self, kudp_buffer *buffer);
+void kudp_close(kudp_socket *self);
+
+#endif
+
+#ifdef KUDP_IMPLEMENTATION
+#undef KUDP_IMPLEMENTATION
+
+kudp_socket *kudp_connect(const char *address, unsigned short port, bool use_connect) {
+	char portstr[6];
+	
+	snprintf(portstr, sizeof portstr, "%hu", port);
+	
+	kudp_socket *self = malloc(sizeof *self);
+	
+	if (!self) {
+		return self;
+	}
+	
+	struct addrinfo hints = {
+		.ai_family = AF_UNSPEC,
+		.ai_socktype = SOCK_DGRAM,
+		.ai_protocol = 0,
+		.ai_flags = AI_ADDRCONFIG | AI_NUMERICHOST,
+	};
+	
+	struct addrinfo *results = NULL;
+	
+	if (getaddrinfo(address, portstr, &hints, &results) || !results) {
+		free(self);
+		return NULL;
+	}
+	
+	// Open socket
+	self->fd = socket(results->ai_family, results->ai_socktype, results->ai_protocol);
+	
+	if (self->fd == -1) {
+		free(self); freeaddrinfo(results);
+		return NULL;
+	}
+	
+	// Set send and recieve address
+	if (use_connect) {
+		if (connect(self->fd, results->ai_addr, results->ai_addrlen)) {
+			free(self); freeaddrinfo(results);
+			return NULL;
+		}
+	}
+	// Set recieve address only
+	else {
+		if (bind(self->fd, results->ai_addr, results->ai_addrlen)) {
+			free(self); freeaddrinfo(results);
+			return NULL;
+		}
+	}
+	
+	freeaddrinfo(results);
+	return self;
+}
+
+kudp_buffer *kudp_recieve(kudp_socket *self) {
+	ssize_t recvsize = recv(self->fd, self->buffer.data, KUDP_MAX_SIZE, MSG_DONTWAIT);
+	
+	if (recvsize == -1) {
+		return NULL;
+	}
+	
+	self->buffer.size = recvsize;
+	
+	return &self->buffer;
+}
+
+bool kudp_send(kudp_socket *self, kudp_buffer *buffer) {
+	ssize_t nsent = send(self->fd, buffer->data, buffer->size, MSG_DONTWAIT);
+	
+	return nsent == buffer->size;
+}
+
+void kudp_close(kudp_socket *self) {
+	shutdown(self->fd, SHUT_RDWR);
+	close(self->fd);
+	free(self);
+}
+
+#endif
